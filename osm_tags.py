00dbd7944928e87afac7daddc53c807f28bef04f
Jon Schleuss <gaufre@gmail.com>
2016-06-20 12:39:54 -0700

    this work?

--- osm_tags.py
+++ osm_tags.py
@@ -14,11 +14,25 @@ def csv_to_json(mapping_name, csv_file):
     reader.next()  # skip header row
     mappings[mapping_name] = {}
     for row in reader:
+        # print row
         if row[1] != '' and row[2] != '':
             mappings[mapping_name][row[0]] = {
                 'key1': row[1],
                 'val1': row[2]
             }
+            if len(row) == 5:
+                if row[3] != '' and row[4] != '':
+                    mappings[mapping_name][row[0]] = {
+                        'key2': row[3],
+                        'val2': row[4]
+                    }
+            if len(row) == 7:
+                if row[5] != '' and row[6] != '':
+                    mappings[mapping_name][row[0]] = {
+                        'key3': row[4],
+                        'val3': row[5]
+                    }
+    # print mappings
 
 csv_to_json('GeneralUse', GENERAL_USE_CSV)
 csv_to_json('Specific_1', SPECIFIC_1_CSV)
